83ff9baa99a5691c9884969b14a8b78c944b4cef
Emmanuel P. Dela Peña <168615722+Tis-Emman@users.noreply.github.com>
2024-06-18 21:48:11 +0800

    tambak

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student {
+
+    String name;
+    int age = 17;
+    String status = "Single";
+    String address = "Sabang, Baliuag, Bulacan";
+    String birthdate = "October 25, 2006";
+
+    public void setName(String newName){
+
+    }
+
+    
+
+    
+}
