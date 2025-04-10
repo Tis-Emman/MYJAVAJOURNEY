603d9929ddf318a04175662ee21278504c71a2e8
Emmanuel <delapenaenrico018@gmail.com>
2025-04-10 10:40:12 +0800

    Enums

---
+++ src/main/java/com/mycompany/mavenproject4/Bird.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject4;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Bird implements Actions {
+
+    public String name;
+    public String age;
+    
+    Bird(){
+    }
+    
+    public void sayHello(){
+        
+    }
+   
+}
---
+++ src/main/java/com/mycompany/mavenproject4/Planet.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.mavenproject4;
+
+/**
+ *
+ * @author ADMIN
+ */
+public enum Planet {
+    MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE
+    
+    
+}
