662b10ddc14c08091bc585d41e57b993e1929fa9
Emmanuel P. Dela Peña <168615722+Tis-Emman@users.noreply.github.com>
2025-04-11 22:46:56 +0800

    Update student.java

--- student.java
+++ student.java
@@ -1,7 +1,7 @@
 public class student {
 
     String name;
-    int age = 17;
+    int age = 18;
     String status = "Single";
     String address = "Sabang, Baliuag, Bulacan";
     String birthdate = "October 25, 2006";
